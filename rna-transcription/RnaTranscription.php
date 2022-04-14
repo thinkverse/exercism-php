bdb4a1d521979e0e11c79b943226600fc81ac817
thinkverse <hallberg.kim@gmail.com>
2022-04-14 09:34:29 +0200

    Fix outdated `Rna Transcription` solution

--- rna-transcription/RnaTranscription.php
+++ rna-transcription/RnaTranscription.php
@@ -26,5 +26,7 @@ declare(strict_types=1);
 
 function toRna(string $dna): string
 {
-    throw new \BadFunctionCallException("Implement the toRna function");
+    $map = ['G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'];
+
+    return implode('', array_map(fn ($nucleotide) => $map[$nucleotide], mb_str_split($dna)));
 }
